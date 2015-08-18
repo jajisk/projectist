4288b4d0c1fa388849eb292660dd7d44a0ae4882
jajisk <jajiskuk@gmail.com>
2015-08-18 13:50:40 +0100

    Initial Commit

---
+++ app/dependencies.php
@@ -0,0 +1,45 @@
+<?php
+// DIC configuration
+
+$container = $app->getContainer();
+
+// -----------------------------------------------------------------------------
+// Service providers
+// -----------------------------------------------------------------------------
+
+// Twig
+$container['view'] = function ($c) {
+    $view = new \Slim\Views\Twig($c['settings']['view']['template_path'], $c['settings']['view']['twig']);
+
+    // Add extensions
+    $view->addExtension(new Slim\Views\TwigExtension($c['router'], $c['request']->getUri()));
+    $view->addExtension(new Twig_Extension_Debug());
+
+    return $view;
+};
+
+// Flash messages
+$container['flash'] = function ($c) {
+    return new \Slim\Flash\Messages;
+};
+
+// -----------------------------------------------------------------------------
+// Service factories
+// -----------------------------------------------------------------------------
+
+// monolog
+$container['logger'] = function ($c) {
+    $settings = $c['settings']['logger'];
+    $logger = new \Monolog\Logger($settings['name']);
+    $logger->pushProcessor(new \Monolog\Processor\UidProcessor());
+    $logger->pushHandler(new \Monolog\Handler\StreamHandler($settings['path'], \Monolog\Logger::DEBUG));
+    return $logger;
+};
+
+// -----------------------------------------------------------------------------
+// Action factories
+// -----------------------------------------------------------------------------
+
+$container['App\Action\HomeAction'] = function ($c) {
+    return new App\Action\HomeAction($c['view'], $c['logger']);
+};
